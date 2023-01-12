6dd70b2c757437f1bc332deb59c887281b234e12
Matheus Neves <matneves@SAO-C02G620KML7H.local>
2023-01-11 22:02:51 -0300

    feat(styles/global): import global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { ThemeProvider } from "styled-components";
 import { Transactions } from "./pages/Transactions";
+import { GlobalStyle } from "./styles/global";
 import { defaultTheme } from "./styles/themes/default";
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
       <Transactions />
     </ThemeProvider>
   )
