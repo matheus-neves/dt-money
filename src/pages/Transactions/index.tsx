1f4f3e3c11de547ff6ade40974b51bfef9132cec
Matheus Neves <matneves@SAO-C02G620KML7H.local>
2023-01-11 22:04:14 -0300

    feat(components/transaction-table): create/style component

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,11 +1,40 @@
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
+import { PriceHighlight, TransactionsContainer, TransactionsTable } from "./styles";
 
 export function Transactions() {
   return (
     <div>
       <Header />
       <Summary />
+
+      <TransactionsContainer as="main">
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width='50%'>Site development</td>
+              <td>
+                <PriceHighlight variant="income">
+                  USD 3,000
+                </PriceHighlight>
+              </td>
+              <td>Sale</td>
+              <td>04/13/2022</td>
+            </tr>
+            <tr>
+              <td width='50%'>Hamburger</td>
+              <td>
+                <PriceHighlight variant="outcome">
+                  - USD 59,00
+                </PriceHighlight>
+              </td>
+              <td>Food</td>
+              <td>04/10/2022</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
+
     </div>
   )
 }
