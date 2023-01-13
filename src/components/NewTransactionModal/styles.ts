ea8b945aaadba30e2f59fcf73da88de42680f0b4
Matheus Neves <matheusneves1989@gmail.com>
2023-01-13 19:15:29 -0300

    feat(components/transaction-type): create/style transaction type button

--- src/components/NewTransactionModal/styles.ts
+++ src/components/NewTransactionModal/styles.ts
@@ -1,4 +1,5 @@
 import * as Dialog from '@radix-ui/react-dialog';
+import * as RadioGroup from '@radix-ui/react-radio-group';
 import styled from 'styled-components';
 
 export const Overlay = styled(Dialog.Overlay)`
@@ -67,4 +68,43 @@ export const CloseButton = styled(Dialog.Close)`
   line-height: 0;
   cursor: pointer;
   color: ${props => props.theme['green-500']};
+`
+
+export const TransactionType = styled(RadioGroup.Root)`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  gap: 1rem;
+  margin-top: 0.5rem;
+`;
+
+
+export const TransactionTypeButton = styled(RadioGroup.Item)`
+  background-color: ${props => props.theme["gray-700"]};
+  padding: 1rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 0.5rem;
+  border-radius: 6px;
+  cursor: pointer;
+  border: 0;
+  color: ${props => props.theme['gray-300']};
+
+  svg {
+    color: ${props => props.value === 'income' ? props.theme['green-300'] : props.theme['red-300']};
+  }
+
+  &[data-state='unchecked']:hover {
+    transform: background-color 0.2s;
+    background-color: ${props => props.theme['gray-600']};
+  }
+
+  &[data-state='checked'] {
+    color: ${props => props.theme.white};
+    background-color: ${props => props.value === 'income' ? props.theme["green-500"] : props.theme["red-500"]};
+
+    svg {
+      color: ${props => props.theme.white};
+    }
+  }
 `
